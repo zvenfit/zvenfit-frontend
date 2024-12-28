1f525a16dc38b5c47552d7e97fd32c192ded3462
Nelmad <nelmadmorrey@gmail.com>
2024-12-28 13:52:48 +0300

    group training modals

--- src/pages/main/containers/Application/Application.tsx
+++ src/pages/main/containers/Application/Application.tsx
@@ -4,7 +4,11 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 const DevelopmentApp = React.lazy(() =>
   import('../../components/DevelopmentApp').then(({ DevelopmentApp }) => ({ default: DevelopmentApp })),
 );
+import { DanceFitnessModal } from '../../components/group-training-modals/DanceFitnessModal/DanceFitnessModal';
+import { StepAerobicsModal } from '../../components/group-training-modals/StepAerobicsModal';
 import { StrengthTrainingModal } from '../../components/group-training-modals/StrengthTrainingModal';
+import { StretchingModal } from '../../components/group-training-modals/StretchingModal';
+import { YogalatesModal } from '../../components/group-training-modals/YogalatesModal';
 import { MODALS_URLS } from '../../constants/modalsUrls';
 
 export const Application: React.FC = () => {
@@ -19,10 +23,10 @@ export const Application: React.FC = () => {
       <HashRouter>
         <Routes>
           <Route path={MODALS_URLS.STRENGTH_TRAINING} Component={StrengthTrainingModal} />
-          <Route path={MODALS_URLS.STEP_AEROBICS} Component={StrengthTrainingModal} />
-          <Route path={MODALS_URLS.DANCE_FITNESS} Component={StrengthTrainingModal} />
-          <Route path={MODALS_URLS.YOGALATES} Component={StrengthTrainingModal} />
-          <Route path={MODALS_URLS.STRETCHING} Component={StrengthTrainingModal} />
+          <Route path={MODALS_URLS.STEP_AEROBICS} Component={StepAerobicsModal} />
+          <Route path={MODALS_URLS.DANCE_FITNESS} Component={DanceFitnessModal} />
+          <Route path={MODALS_URLS.YOGALATES} Component={YogalatesModal} />
+          <Route path={MODALS_URLS.STRETCHING} Component={StretchingModal} />
           <Route path="*" element={null} />
         </Routes>
       </HashRouter>
