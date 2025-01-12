6f08aae83d9a54931819566bdf8e6407cc1ff9cb
ADereglazov <sanya95@yandex.ru>
2025-01-12 17:58:27 +0300

    style -> Logo.module.css

--- src/common/components/Logo/Logo.tsx
+++ src/common/components/Logo/Logo.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import * as styles from './Logo.module.css';
+
 export const Logo: React.FC<{
   className?: string;
 }> = ({ className }) => {
   return (
-    <div className={className}>
-      <span style={{ color: '#54c263' }}>ZVEN</span>FIT
+    <div className={`${styles['logo']} ${className}`}>
+      <span className={styles['logo__text--highlighted']}>ZVEN</span>FIT
     </div>
   );
 };
