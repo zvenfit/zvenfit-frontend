c43a900a0aaa2f8514ebfd56841d8617603901b1
ADereglazov <sanya95@yandex.ru>
2025-01-12 18:13:12 +0300

    Logo component to wrapper div, removed props from Logo.tsx

--- src/common/components/Footer/Footer.tsx
+++ src/common/components/Footer/Footer.tsx
@@ -8,7 +8,9 @@ export const Footer: React.FC = () => {
   return (
     <footer className={styles['footer']}>
       <div className={styles['footer__container']}>
-        <Logo className={styles['footer__logo']} />
+        <div className={styles['footer__logo']}>
+          <Logo />
+        </div>
 
         <div className={styles['footer__socials-container']}>
           <nav className={styles['footer__socials']}>
--- src/common/components/Logo/Logo.tsx
+++ src/common/components/Logo/Logo.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 import * as styles from './Logo.module.css';
 
-export const Logo: React.FC<{
-  className?: string;
-}> = ({ className }) => {
+export const Logo: React.FC = () => {
   return (
-    <div className={`${styles['logo']} ${className}`}>
+    <div className={styles['logo']}>
       <span className={styles['logo__text--highlighted']}>ZVEN</span>FIT
     </div>
   );
