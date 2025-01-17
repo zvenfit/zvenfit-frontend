1ebda09b6767dbb9cf8ed825d393622cb5816201
ADereglazov <sanya95@yandex.ru>
2025-01-17 15:20:44 +0300

    zoom for Yandex-map

--- src/common/components/Contacts/components/Map/Map.tsx
+++ src/common/components/Contacts/components/Map/Map.tsx
@@ -5,7 +5,7 @@ import * as styles from './Map.module.css';
 import { MapRoot } from '../../../../../packages/ymaps3/components/MapRoot';
 import { YMapDefaultSchemeLayer } from '../../../../../packages/ymaps3/components/YMapDefaultSchemeLayer';
 
-const DEFAULT_LOCATION: YMapLocationRequest = { center: [36.85064, 55.733647], zoom: 14 };
+const DEFAULT_LOCATION: YMapLocationRequest = { center: [36.85064, 55.733647], zoom: 16 };
 
 export const Map: React.FC = () => {
   return (
