35373399cbc10585ef474e3930c5303efcdd6de9
ADereglazov <sanya95@yandex.ru>
2025-01-21 23:36:39 +0300

    trainer list styling skew

--- src/common/components/TrainerList/TrainerList.tsx
+++ src/common/components/TrainerList/TrainerList.tsx
@@ -5,11 +5,15 @@ import * as styles from './TrainerList.module.css';
 export const TrainerList: React.FC = () => {
   return (
     <section className={styles['trainer-list']}>
+      <div className={styles['trainer-list__skew']} />
+
       <header className={styles['trainer-list__header']}>
         <h2 className={styles['trainer-list__title']}>Тренерский состав</h2>
       </header>
 
       <div>Фотки</div>
+
+      <div className={styles['trainer-list__skew']} />
     </section>
   );
 };
