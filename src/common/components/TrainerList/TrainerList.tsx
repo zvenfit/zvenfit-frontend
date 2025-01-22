bafc82785625cc86541615442d9c77620d0e8adb
ADereglazov <sanya95@yandex.ru>
2025-01-22 11:22:14 +0300

    trainer list styling

--- src/common/components/TrainerList/TrainerList.tsx
+++ src/common/components/TrainerList/TrainerList.tsx
@@ -7,11 +7,13 @@ export const TrainerList: React.FC = () => {
     <section className={styles['trainer-list']}>
       <div className={styles['trainer-list__skew']} />
 
-      <header className={styles['trainer-list__header']}>
-        <h2 className={styles['trainer-list__title']}>Тренерский состав</h2>
-      </header>
+      <div className={`container ${styles['trainer-list__container']}`}>
+        <header className={styles['trainer-list__header']}>
+          <h2 className={styles['trainer-list__title']}>Тренерский состав</h2>
+        </header>
 
-      <div>Фотки</div>
+        <div>Фотки</div>
+      </div>
 
       <div className={styles['trainer-list__skew']} />
     </section>
