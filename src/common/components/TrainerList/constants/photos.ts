61c15ab98e753079a90bc861b6b55dd8f3c9a23b
ADereglazov <sanya95@yandex.ru>
2025-01-22 13:43:25 +0300

    add constant photos.ts

---
+++ src/common/components/TrainerList/constants/photos.ts
@@ -0,0 +1,18 @@
+export const PHOTOS = [
+  {
+    name: 'Елена',
+    src: 'https://259506.selcdn.ru/sites-static/site959140/ee2f63cb-b0e9-4813-9f51-a9de4370cb61/ee2f63cb-b0e9-4813-9f51-a9de4370cb61-10816184.jpeg',
+  },
+  {
+    name: 'Ефим',
+    src: 'https://259506.selcdn.ru/sites-static/site959140/b22366d0-127e-48a7-881d-50ff26dd357b/b22366d0-127e-48a7-881d-50ff26dd357b-10816192.jpeg',
+  },
+  {
+    name: 'Оксана',
+    src: 'https://259506.selcdn.ru/sites-static/site959140/24c4f6cf-c39e-4d33-bec8-b43a50e59d71/24c4f6cf-c39e-4d33-bec8-b43a50e59d71-11064161.jpeg',
+  },
+  {
+    name: 'Елена',
+    src: 'https://259506.selcdn.ru/sites-static/site959140/0e435186-2a20-4213-a6c6-92ac053099c5/0e435186-2a20-4213-a6c6-92ac053099c5-11064420.jpeg',
+  },
+];
