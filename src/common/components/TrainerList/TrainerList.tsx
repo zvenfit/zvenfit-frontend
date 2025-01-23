a35f67738b50a27dc7f99759915f777600d37c05
ADereglazov <sanya95@yandex.ru>
2025-01-23 16:30:08 +0300

    layout for TrainerList.tsx

--- src/common/components/TrainerList/TrainerList.tsx
+++ src/common/components/TrainerList/TrainerList.tsx
@@ -8,12 +8,14 @@ export const TrainerList: React.FC = () => {
     <section className={styles['trainer-list']}>
       <div className={styles['trainer-list__skew']} />
 
-      <div className={`container ${styles['trainer-list__container']}`}>
-        <header className={styles['trainer-list__header']}>
-          <h2 className={styles['trainer-list__title']}>Тренерский состав</h2>
-        </header>
+      <div className={'container'}>
+        <div className={styles['trainer-list__wrapper']}>
+          <header className={styles['trainer-list__header']}>
+            <h2 className={styles['trainer-list__title']}>Тренерский состав</h2>
+          </header>
 
-        <Slider />
+          <Slider />
+        </div>
       </div>
 
       <div className={styles['trainer-list__skew']} />
