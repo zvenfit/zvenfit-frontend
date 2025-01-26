b7309e47985e2597379fbe49c2262ab7ebbe33f8
ADereglazov <sanya95@yandex.ru>
2025-01-26 18:57:19 +0300

    styles

--- src/common/containers/AdvantagesContainer/components/Advantages.tsx
+++ src/common/containers/AdvantagesContainer/components/Advantages.tsx
@@ -12,22 +12,22 @@ export const Advantages: React.FC<AdvantagesProps> = ({ content }) => {
     <section className={styles['advantages']}>
       <h2 className={'visually-hidden'}>Преимущества</h2>
 
-      <ul className={styles['advantages__wrapper']}>
+      <ul className={styles['advantages__list']}>
         {content.map((item: IContent) => {
           return (
             <li
               key={item.title}
               style={{ backgroundImage: `url(${item.bgImage})` }}
-              className={styles['advantages__item']}
+              className={styles['advantages__list-item']}
             >
-              <h3 className={styles['advantages__item-title']}>{item.title}</h3>
+              <h3 className={styles['advantages__list-item-title']}>{item.title}</h3>
 
-              <p className={styles['advantages__item-description']}>{item.description}</p>
+              <p className={styles['advantages__list-item-description']}>{item.description}</p>
 
               <button
                 type={'button'}
                 style={{ backgroundColor: item.btnColor }}
-                className={styles['advantages__item-button']}
+                className={styles['advantages__list-item-button']}
               >
                 Подробнее
               </button>
