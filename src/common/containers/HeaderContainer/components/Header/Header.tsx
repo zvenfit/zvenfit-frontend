b71b6c031339df44411aa9c9075c63b4fddbae47
ADereglazov <sanya95@yandex.ru>
2025-01-31 18:00:28 +0300

    scroll to the section by clicking on the menu item

--- src/common/containers/HeaderContainer/components/Header/Header.tsx
+++ src/common/containers/HeaderContainer/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 
 import * as styles from './Header.module.css';
 import { LOGO_URL } from '../../../../../constants/common';
@@ -15,9 +16,11 @@ export const Header: React.FC<HeaderProps> = ({ menuItems }) => {
   return (
     <header className={`${styles['header']} ${menuOpened && styles['header--menu-opened']}`}>
       <nav className={`container ${styles['header__nav']}`}>
-        <a href="#" target="_self" className={styles['header__logo']}>
-          <img src={LOGO_URL} alt="Логотип" width="105" height="30" className={styles['header__logo-img']} />
-        </a>
+        <Router>
+          <NavLink to="/" className={styles['header__logo']}>
+            <img src={LOGO_URL} alt="Логотип" width="105" height="30" className={styles['header__logo-img']} />
+          </NavLink>
+        </Router>
 
         <ul className={`${styles['header__menu']} ${menuOpened && styles['header__menu--active']}`}>
           {menuItems.map(item => {
