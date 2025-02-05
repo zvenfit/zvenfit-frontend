b1eee7384d30d4af80d07b6ba5eb272581f44e04
ADereglazov <sanya95@yandex.ru>
2025-02-06 00:10:39 +0300

    add index.ts to HeaderContainer folder

--- src/pages/main/containers/Application/Application.tsx
+++ src/pages/main/containers/Application/Application.tsx
@@ -8,7 +8,7 @@ import { ContactsContainer } from '../../../../common/containers/ContactsContain
 import { DescriptionContainer } from '../../../../common/containers/DescriptionContainer';
 import { FooterContainer } from '../../../../common/containers/FooterContainer';
 import { GalleryContainer } from '../../../../common/containers/GalleryContainer';
-import { HeaderContainer } from '../../../../common/containers/HeaderContainer/HeaderContainer';
+import { HeaderContainer } from '../../../../common/containers/HeaderContainer';
 import { HeroContainer } from '../../../../common/containers/HeroContainer';
 import { TrainingDirectionsContainer } from '../../../../common/containers/TrainingDirectionsContainer';
 import { TrainingListContainer } from '../../../../common/containers/TrainingListContainer';
