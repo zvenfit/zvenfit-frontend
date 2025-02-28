2840c3dd65a1a2a36302e98fa698e187135f5d62
ADereglazov <sanya95@yandex.ru>
2025-02-28 11:38:27 +0300

    fix styles

--- src/common/containers/RegistrationFormContainer/components/InfoText/InfoText.tsx
+++ src/common/containers/RegistrationFormContainer/components/InfoText/InfoText.tsx
@@ -7,25 +7,24 @@ export const InfoText: React.FC = () => {
     <>
       <div className={styles['info-text__title-block']}>
         <b className={styles['info-text__text']}>
-          Запишись<span className={styles['info-text__text--green']}> сегодня!</span>
+          Запишись<span className="green-text"> сегодня!</span>
         </b>
 
         <p className={styles['info-text__text']}>
-          <span className={styles['info-text__text--green']}>2 групповые </span>
-          тренировки по одному из пяти направлений по
-          <span className={styles['info-text__text--green']}> 299 ₽</span>
+          <span className="green-text">2 групповые</span> тренировки по одному из пяти направлений по
+          <span className="green-text"> 299 ₽</span>
         </p>
       </div>
 
       <div className={styles['info-text__text-block']}>
-        <p className={styles['info-text__text--gray']}>*старая цена - 800 ₽ за тренировку</p>
+        <p className="gray-text">*старая цена - 800 ₽ за тренировку</p>
         <br />
         <p>
-          <span className={styles['info-text__text--green']}>Оставьте контакты</span>
-          <span className={styles['info-text__text--gray']}>, и наш менеджер свяжется с вами</span>
-          <span className={styles['info-text__text--green']}> для записи</span>
-          <span className={styles['info-text__text--gray']}> на любое интересное для вас занятие и ответит на</span>
-          <span className={styles['info-text__text--green']}> все ваши вопросы</span>
+          <span className="green-text">Оставьте контакты</span>
+          <span className="gray-text">, и наш менеджер свяжется с вами</span>
+          <span className="green-text"> для записи</span>
+          <span className="gray-text"> на любое интересное для вас занятие и ответит на</span>
+          <span className="green-text"> все ваши вопросы</span>
         </p>
       </div>
     </>
