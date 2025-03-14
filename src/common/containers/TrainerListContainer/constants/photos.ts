389d3ee006eac147469ee9bcb6d22038f874fb35
ADereglazov <sanya95@yandex.ru>
2025-03-14 22:23:08 +0300

    images to yandex-cloud

--- src/common/containers/TrainerListContainer/constants/photos.ts
+++ src/common/containers/TrainerListContainer/constants/photos.ts
@@ -3,18 +3,18 @@ import { IPhoto } from '../components/Slider';
 export const PHOTOS: IPhoto[] = [
   {
     name: 'Елена',
-    src: 'https://259506.selcdn.ru/sites-static/site959140/ee2f63cb-b0e9-4813-9f51-a9de4370cb61/ee2f63cb-b0e9-4813-9f51-a9de4370cb61-10816184.jpeg',
+    src: 'https://storage.yandexcloud.net/zvenfit/static-images/trainer-list/01-elena.jpeg',
   },
   {
     name: 'Ефим',
-    src: 'https://259506.selcdn.ru/sites-static/site959140/b22366d0-127e-48a7-881d-50ff26dd357b/b22366d0-127e-48a7-881d-50ff26dd357b-10816192.jpeg',
+    src: 'https://storage.yandexcloud.net/zvenfit/static-images/trainer-list/02-efim.jpeg',
   },
   {
     name: 'Оксана',
-    src: 'https://259506.selcdn.ru/sites-static/site959140/24c4f6cf-c39e-4d33-bec8-b43a50e59d71/24c4f6cf-c39e-4d33-bec8-b43a50e59d71-11064161.jpeg',
+    src: 'https://storage.yandexcloud.net/zvenfit/static-images/trainer-list/03-oksana.jpeg',
   },
   {
     name: 'Елена',
-    src: 'https://259506.selcdn.ru/sites-static/site959140/0e435186-2a20-4213-a6c6-92ac053099c5/0e435186-2a20-4213-a6c6-92ac053099c5-11064420.jpeg',
+    src: 'https://storage.yandexcloud.net/zvenfit/static-images/trainer-list/04-elena.jpeg',
   },
 ];
--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,6 +1,5 @@
 // TODO позже заменить
-export const LOGO_URL =
-  'https://259506.selcdn.ru/sites-static/site959140/3861673f-1856-473f-84bf-11eaee774160/3861673f-1856-473f-84bf-11eaee774160-10815684.png';
+export const LOGO_URL = 'https://storage.yandexcloud.net/zvenfit/static-images/logo/logo.png';
 
 // Использовать в качестве фолбека, хост достаём из env переменных
 export const HOST_DEFAULT = 'zvenfit.ru';
