5abd405cee02953b69a70060069de49201d0cd83
ADereglazov <sanya95@yandex.ru>
2025-03-23 10:07:37 +0300

    svg for FloatingButton to component ArrowIcon

--- src/common/components/FloatingButton/FloatingButton.tsx
+++ src/common/components/FloatingButton/FloatingButton.tsx
@@ -2,6 +2,7 @@ import { clsx } from 'clsx';
 import React, { useEffect, useState } from 'react';
 
 import * as styles from './FloatingButton.modules.css';
+import { ArrowIcon } from './icons/ArrowIcon';
 import { scrollToTop } from '../../../packages/utils';
 
 export const FloatingButton: React.FC = () => {
@@ -28,14 +29,7 @@ export const FloatingButton: React.FC = () => {
       className={clsx(styles['floating-button'], !isVisible && styles['floating-button--hidden'])}
       onClick={scrollToTop}
     >
-      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32">
-        <path
-          fillRule="evenodd"
-          clipRule="evenodd"
-          d="M7.057 14.276c.521.52 1.365.52 1.886 0l5.724-5.724v18.189c0 .695.597 1.259 1.333 1.259s1.334-.564 1.334-1.26V8.553l5.723 5.724a1.333 1.333 0 1 0 1.886-1.886l-8-8a1.333 1.333 0 0 0-1.886 0l-8 8c-.52.521-.52 1.365 0 1.886z"
-          fill="#ffffff"
-        />
-      </svg>
+      <ArrowIcon />
     </button>
   );
 };
