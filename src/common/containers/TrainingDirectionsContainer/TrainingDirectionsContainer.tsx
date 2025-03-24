b6438448fb70dc8a80cb4d18b2d5bfe32403e1d6
Nelmad <nelmadmorrey@gmail.com>
2025-03-24 15:22:49 +0300

    open modals for training directions

--- src/common/containers/TrainingDirectionsContainer/TrainingDirectionsContainer.tsx
+++ src/common/containers/TrainingDirectionsContainer/TrainingDirectionsContainer.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { TrainingDirections } from './components/TrainingDirections';
+import { Layout } from './components/Layout';
+import { TrainingDirectionBlock } from './components/TrainingDirectionBlock';
 import { CONTENT } from './constants/content';
 
 export const TrainingDirectionsContainer: React.FC = () => {
-  return <TrainingDirections content={CONTENT} />;
+  const navigate = useNavigate();
+  const handleClick = React.useCallback(
+    (href: string) => {
+      navigate(href);
+    },
+    [navigate],
+  );
+
+  return (
+    <Layout
+      listItems={CONTENT.map(item => (
+        <TrainingDirectionBlock
+          href={item.href}
+          key={item.title}
+          title={item.title}
+          description={item.description}
+          btnTheme={item.btnTheme}
+          image={item.bgImage}
+          onClick={handleClick}
+        />
+      ))}
+    />
+  );
 };
--- src/common/containers/TrainingDirectionsContainer/types.ts
+++ src/common/containers/TrainingDirectionsContainer/types.ts
@@ -1,6 +1,7 @@
-import { TTheme } from '../../../../components/Button/types';
+import { TTheme } from '../../components/Button/types';
 
 export interface IContent {
+  href: string;
   bgImage: string;
   title: string;
   description: string[];
