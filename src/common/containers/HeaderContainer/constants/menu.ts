0fcee240579230295619a8b58cd66c18c83089e0
ADereglazov <sanya95@yandex.ru>
2025-04-01 10:12:24 +0300

    Использование глобального якоря для секций

--- src/common/containers/HeaderContainer/constants/menu.ts
+++ src/common/containers/HeaderContainer/constants/menu.ts
@@ -1,16 +1,17 @@
+import { CONTACTS_ID, DIRECTIONS_ID, GALLERY_ID } from '../../../../pages/main/constants/pageAnchors';
 import { IMenuItem } from '../components/Header';
 
 export const MENU: IMenuItem[] = [
   {
     title: 'Направления тренировок',
-    link: '#directions',
+    link: `#${DIRECTIONS_ID}`,
   },
   {
     title: 'Галерея',
-    link: '#gallery',
+    link: `#${GALLERY_ID}`,
   },
   {
     title: 'Контакты',
-    link: '#contacts',
+    link: `#${CONTACTS_ID}`,
   },
 ];
